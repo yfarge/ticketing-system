74a59cf5a048c9d02c9d221899367c210ebebf23
Yousef Farge <yousef.farge@gmail.com>
2024-05-09 23:33:19 -0500

    feat(drizzle): configured db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default {
   schema: "./src/server/db/schema.ts",
   driver: "pg",
   dbCredentials: {
-    connectionString: env.DATABASE_URL,
+    connectionString: env.POSTGRES_URL,
   },
   tablesFilter: ["ticketing-system_*"],
 } satisfies Config;
